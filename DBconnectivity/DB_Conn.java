c1fd36c26a35e95179b8ca7f2fe575ac79890203
Amit Wanare <wanareamit19@gmail.com>
2023-03-24 14:41:24 +0530

    DataBase Connectivity JDBC

--- DBconnectivity/DB_Conn.java
+++ DBconnectivity/DB_Conn.java
@@ -12,7 +12,7 @@ public class DB_Conn {
             while(rs.next()){
                 System.out.println(rs.getInt(1)+" "+ rs.getString(2)+" "+ rs.getString(3)+" "+rs.getString(4)+" "
                         +rs.getInt(5)+" "+rs.getInt(6)+" "+rs.getString(7) );
-               // cnn.close();
+                //cnn.close();
             }
 //            st.execute(insert);
 //            cnn.close();
